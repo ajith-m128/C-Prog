7872b92cda243783484b84462acc8c3eb141952a
ajith-m128 <46275398+ajith-m128@users.noreply.github.com>
2019-02-08 17:41:10 +0530

    Create Odd.c

---
+++ Odd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+	printf("even number");
+  }
+else if(n%2==1)
+{
+	printf("odd number");
+}
+	else
+	printf("Invalid");
+return 0;
+}
